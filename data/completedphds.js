e44001cd6bd96124e90d5400c5b23376360fe7b1
Zeeshan Akhtar <zshanakhtar@outlook.com>
2021-09-02 17:12:31 +0530

    completed phd data update

--- data/completedphds.js
+++ data/completedphds.js
@@ -2,7 +2,7 @@ const completedphds =[
     {
         author:"Neelam Dwivedi [2015RCS02]",
         date:"28 August 2021",
-        status:"Thesis Completed",
+        status:"PhD Completed",
         title:"Novel Techniques for Suspicious Activity Detection through Video Surveillance at Public Places",
     },
 ];
